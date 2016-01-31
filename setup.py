88e121c30fac3d68dfd662add477186d656e6be8
Idan Kamara <idankk86@gmail.com>
2016-01-31 13:32:41 -0800

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bashlex',
-    version='0.6',
+    version='0.8',
     url='https://github.com/idank/bashlex.git',
     license='GPLv3+',
     author='Idan Kamara',
