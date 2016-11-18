idank/bashlex

commit 4e091dd066d3c44bb031a2f44d8ea14d5122f9fd

parser: add a knob to limit the amount of recursive parsing

bashlex/parser.py
@@ -99,30 +99,50 @@ def p_redirection(p):
                    | REDIR_WORD LESS_AND DASH
                    | AND_GREATER WORD
                    | AND_GREATER_GREATER WORD'''
+    parserobj = p.context
     if len(p) == 3:
         output = p[2]
         if p.slice[2].ttype == tokenizer.tokentype.WORD:
-            output = _expandword(p.lexer, p.slice[2])
+            output = _expandword(parserobj, p.slice[2])
         p[0] = ast.node(kind='redirect', input=None, type=p[1], heredoc=None,
                         output=output, pos=(p.lexpos(1), p.endlexpos(2)))
     else:
         output = p[3]
         if p.slice[3].ttype == tokenizer.tokentype.WORD:
-            output = _expandword(p.lexer, p.slice[3])
+            output = _expandword(parserobj, p.slice[3])
         p[0] = ast.node(kind='redirect', input=p[1], type=p[2], heredoc=None,
                         output=output, pos=(p.lexpos(1), p.endlexpos(3)))
 
-def _expandword(tokenizer, tokenword):
-    quoted = bool(tokenword.flags & flags.word.QUOTED)
-    doublequoted = quoted and tokenword.value[0] == '"'
+def _expandword(parser, tokenword):
+    if parser._expansionlimit == -1:
+        # we enter this branch in the following conditions:
+        # - currently parsing a substitution as a result of an expansion
+        # - the previous expansion had limit == 0
+        #
+        # this means that this node is a descendant of a substitution in an
+        # unexpanded word and will be filtered in the limit == 0 condition below
+        #
+        # (the reason we even expand when limit == 0 is to get quote removal)
+        node = ast.node(kind='word', word=tokenword,
+                        pos=(tokenword.lexpos, tokenword.endlexpos), parts=[])
+        return node
+    else:
+        quoted = bool(tokenword.flags & flags.word.QUOTED)
+        doublequoted = quoted and tokenword.value[0] == '"'
 
-    # TODO set qheredocument
-    parts, expandedword = subst._expandwordinternal(tokenizer, tokenword, 0,
-                                                    doublequoted, 0, 0)
+        # TODO set qheredocument
+        parts, expandedword = subst._expandwordinternal(parser,
+                                                        tokenword, 0,
+                                                        doublequoted, 0, 0)
 
-    node = ast.node(kind='word', word=expandedword,
-                    pos=(tokenword.lexpos, tokenword.endlexpos), parts=parts)
-    return node
+        # limit reached, don't include substitutions (still expanded to get
+        # quote removal though)
+        if parser._expansionlimit == 0:
+            parts = [node for node in parts if 'substitution' not in node.kind]
+
+        node = ast.node(kind='word', word=expandedword,
+                        pos=(tokenword.lexpos, tokenword.endlexpos), parts=parts)
+        return node
 
 def p_simple_command_element(p):
     '''simple_command_element : WORD
@@ -132,7 +152,8 @@ def p_simple_command_element(p):
         p[0] = [p[1]]
         return
 
-    p[0] = [_expandword(p.lexer, p.slice[1])]
+    parserobj = p.context
+    p[0] = [_expandword(parserobj, p.slice[1])]
 
     # change the word node to an assignment if necessary
     if p.slice[1].ttype == tokenizer.tokentype.ASSIGNMENT_WORD:
@@ -206,7 +227,8 @@ def _makeparts(p):
             parts.extend(p[i])
         elif isinstance(p.slice[i], tokenizer.token):
             if p.slice[i].ttype == tokenizer.tokentype.WORD:
-                parts.append(_expandword(p.lexer, p.slice[i]))
+                parserobj = p.context
+                parts.append(_expandword(parserobj, p.slice[i]))
             else:
                 parts.append(ast.node(kind='reservedword', word=p[i],
                                       pos=p.lexspan(i)))
@@ -514,16 +536,16 @@ for tt in tokenizer.tokentype:
     yaccparser.action[62][tt.name] = -1
     yaccparser.action[63][tt.name] = -141
 
-def parsesingle(s, strictmode=True, convertpos=False):
+def parsesingle(s, strictmode=True, expansionlimit=None, convertpos=False):
     '''like parse, but only consumes a single top level node, e.g. parsing
     'a\nb' will only return a node for 'a', leaving b unparsed'''
-    p = _parser(s, strictmode=strictmode)
+    p = _parser(s, strictmode=strictmode, expansionlimit=expansionlimit)
     tree = p.parse()
     if convertpos:
         ast.posconverter(s).visit(tree)
     return tree
 
-def parse(s, strictmode=True, convertpos=False):
+def parse(s, strictmode=True, expansionlimit=None, convertpos=False):
     '''parse the input string, returning a list of nodes
 
     top level node kinds are:
@@ -534,8 +556,15 @@ def parse(s, strictmode=True, convertpos=False):
     - compound - contains constructs for { list; }, (list), if, for..
 
     leafs are word nodes (which in turn can also contain any of the
-    aforementioned nodes due to command substitutions)'''
-    p = _parser(s, strictmode=strictmode)
+    aforementioned nodes due to command substitutions).
+
+    when strictmode is set to False, we will:
+    - skip reading a heredoc if we're at the end of the input
+
+    expansionlimit is used to limit the amount of recursive parsing done due to
+    command substitutions found during word expansion.
+    '''
+    p = _parser(s, strictmode=strictmode, expansionlimit=expansionlimit)
     parts = [p.parse()]
 
     class endfinder(ast.nodevisitor):
@@ -572,13 +601,12 @@ class _parser(object):
     when we're in the middle of parsing. as a hack, we shove it into the
     YaccProduction context attribute to make it accessible.
     '''
-    def __init__(self, s, strictmode=True, tokenizerargs=None):
-        # when strictmode is set to False, we will:
-        #
-        # - skip reading a heredoc if we're at the end of the input
+    def __init__(self, s, strictmode=True, expansionlimit=None, tokenizerargs=None):
+        assert expansionlimit is None or isinstance(expansionlimit, int)
 
         self.s = s
         self._strictmode = strictmode
+        self._expansionlimit = expansionlimit
 
         if tokenizerargs is None:
             tokenizerargs = {}

bashlex/subst.py
@@ -2,8 +2,11 @@ import copy
 
 from bashlex import ast, flags, tokenizer
 
-def _recursiveparse(tok, base, sindex, tokenizerargs=None):
+def _recursiveparse(parserobj, base, sindex, tokenizerargs=None):
+    # TODO: fix this hack that prevents mutual import
     from bashlex import parser
+
+    tok = parserobj.tok
 
     if tokenizerargs is None:
         tokenizerargs = {'parserstate' : copy.copy(tok._parserstate),
@@ -12,7 +15,11 @@ def _recursiveparse(tok, base, sindex, tokenizerargs=None):
                          'twotokensago' : tok._two_tokens_ago}
 
     string = base[sindex:]
-    p = parser._parser(string, tokenizerargs=tokenizerargs)
+    newlimit = parserobj._expansionlimit
+    if newlimit is not None:
+        newlimit -= 1
+    p = parser._parser(string, tokenizerargs=tokenizerargs,
+                       expansionlimit=newlimit)
     node = p.parse()
 
     endp = node.pos[1]
@@ -20,19 +27,19 @@ def _recursiveparse(tok, base, sindex, tokenizerargs=None):
 
     return node, endp
 
-def _parsedolparen(tok, base, sindex):
-    copiedps = copy.copy(tok._parserstate)
+def _parsedolparen(parserobj, base, sindex):
+    copiedps = copy.copy(parserobj.parserstate)
     copiedps.add(flags.parser.CMDSUBST)
     copiedps.add(flags.parser.EOFTOKEN)
     string = base[sindex:]
 
     tokenizerargs = {'eoftoken' : tokenizer.token(tokenizer.tokentype.RIGHT_PAREN, ')'),
                      'parserstate' : copiedps,
-                     'lastreadtoken' : tok._last_read_token,
-                     'tokenbeforethat' : tok._token_before_that,
-                     'twotokensago' : tok._two_tokens_ago}
+                     'lastreadtoken' : parserobj.tok._last_read_token,
+                     'tokenbeforethat' : parserobj.tok._token_before_that,
+                     'twotokensago' : parserobj.tok._two_tokens_ago}
 
-    node, endp = _recursiveparse(tok, base, sindex, tokenizerargs)
+    node, endp = _recursiveparse(parserobj, base, sindex, tokenizerargs)
 
     if string[endp] != ')':
         while endp > 0 and string[endp-1] == '\n':
@@ -40,20 +47,20 @@ def _parsedolparen(tok, base, sindex):
 
     return node, sindex + endp
 
-def _extractcommandsubst(tok, string, sindex, sxcommand=False):
+def _extractcommandsubst(parserobj, string, sindex, sxcommand=False):
     if string[sindex] == '(':
-        return _extractdelimitedstring(tok, string, sindex, '$(', '(', '(', sxcommand=True)
+        return _extractdelimitedstring(parserobj, string, sindex, '$(', '(', '(', sxcommand=True)
     else:
-        node, si = _parsedolparen(tok, string, sindex)
+        node, si = _parsedolparen(parserobj, string, sindex)
         si += 1
         return ast.node(kind='commandsubstitution', command=node, pos=(sindex-2, si)), si
 
-def _extractprocesssubst(tok, string, sindex):
+def _extractprocesssubst(parserobj, string, sindex):
     #return _extractdelimitedstring(tok, string, sindex, starter, '(', ')', sxcommand=True)
-    node, si = _parsedolparen(tok, string, sindex)
+    node, si = _parsedolparen(parserobj, string, sindex)
     return node, si + 1
 
-def _extractdelimitedstring(tok, string, sindex, opener, altopener, closer,
+def _extractdelimitedstring(parserobj, string, sindex, opener, altopener, closer,
                             sxcommand=False):
     parts = []
     incomment = False
@@ -88,14 +95,14 @@ def _extractdelimitedstring(tok, string, sindex, opener, altopener, closer,
 
         if sxcommand and string[i:i+2] == '$(':
             si = i + 2
-            node, si = _extractcommandsubst(tok, string, si, sxcommand=sxcommand)
+            node, si = _extractcommandsubst(parserobj, string, si, sxcommand=sxcommand)
             parts.append(node)
             i = si + 1
             continue
 
         if string.startswith(opener, i):
             si = i + len(opener)
-            nodes, si = _extractdelimitedstring(tok, string, si, opener, altopener,
+            nodes, si = _extractdelimitedstring(parserobj, string, si, opener, altopener,
                                                 closer, sxcommand=sxcommand)
             parts.extend(nodes)
             i = si + 1
@@ -103,7 +110,7 @@ def _extractdelimitedstring(tok, string, sindex, opener, altopener, closer,
 
         if string.startswith(altopener, i):
             si = i + len(altopener)
-            nodes, si = _extractdelimitedstring(tok, string, si, altopener, altopener,
+            nodes, si = _extractdelimitedstring(parserobj, string, si, altopener, altopener,
                                                 closer, sxcommand=sxcommand)
             parts.extend(nodes)
             i = si + 1
@@ -137,7 +144,7 @@ def _extractdelimitedstring(tok, string, sindex, opener, altopener, closer,
 
     return parts, i
 
-def _paramexpand(tok, string, sindex):
+def _paramexpand(parserobj, string, sindex):
     node = None
     zindex = sindex + 1
     c = string[zindex] if zindex < len(string) else None
@@ -152,7 +159,7 @@ def _paramexpand(tok, string, sindex):
         # TODO
         # return _parameterbraceexpand(string, zindex)
     elif c == '(':
-        return _extractcommandsubst(tok, string, zindex + 1)
+        return _extractcommandsubst(parserobj, string, zindex + 1)
     elif c == '[':
         return _extractarithmeticsubst(string, zindex + 1)
     else:
@@ -180,7 +187,7 @@ def _adjustpositions(node_, base, endlimit):
     visitor = v()
     visitor.visit(node_)
 
-def _expandwordinternal(tok, wordtoken, qheredocument, qdoublequotes, quoted, isexp):
+def _expandwordinternal(parserobj, wordtoken, qheredocument, qdoublequotes, quoted, isexp):
     istring = ''
     parts = []
     tindex = [0]
@@ -210,7 +217,7 @@ def _expandwordinternal(tok, wordtoken, qheredocument, qdoublequotes, quoted, is
             else:
                 tindex = sindex[0] + 1
 
-                node, sindex[0] = _extractprocesssubst(tok, string, tindex)
+                node, sindex[0] = _extractprocesssubst(parserobj, string, tindex)
 
                 parts.append(ast.node(kind='processsubstitution', command=node,
                                       pos=(tindex - 2, sindex[0])))
@@ -256,7 +263,7 @@ def _expandwordinternal(tok, wordtoken, qheredocument, qdoublequotes, quoted, is
 
         elif c == '$' and len(string) > 1:
             tindex = sindex[0]
-            node, sindex[0] = _paramexpand(tok, string, sindex[0])
+            node, sindex[0] = _paramexpand(parserobj, string, sindex[0])
             if node:
                 parts.append(node)
             istring += string[tindex:sindex[0]]
@@ -278,7 +285,7 @@ def _expandwordinternal(tok, wordtoken, qheredocument, qdoublequotes, quoted, is
                         sindex[0] = x
 
                         word = string[tindex+1:sindex[0]]
-                        command, ttindex = _recursiveparse(tok, word, 0)
+                        command, ttindex = _recursiveparse(parserobj, word, 0)
                         _adjustpositions(command, tindex+1, len(string))
                         ttindex += 1 # ttindex is on the closing char
 

tests/test-parser.py
@@ -86,8 +86,8 @@ def whilenode(s, *parts):
     return ast.node(kind='while', parts=list(parts), s=s)
 
 class test_parser(unittest.TestCase):
-    def assertASTEquals(self, s, expected, strictmode=True):
-        results = parse(s, strictmode=strictmode)
+    def assertASTEquals(self, s, expected, **parserargs):
+        results = parse(s, **parserargs)
         self.assertTrue(len(results) == 1, 'expected one ast from parse(), '
                         'got %d' % len(results))
         result = results[0]
@@ -102,8 +102,8 @@ class test_parser(unittest.TestCase):
         msg = 'ASTs not equal for %r\n\nresult:\n\n%s\n\n!=\n\nexpected:\n\n%s' % (s, result.dump(), expected.dump())
         self.assertEquals(result, expected, msg)
 
-    def assertASTsEquals(self, s, expectedlist, strictmode=True):
-        results = parse(s, strictmode=strictmode)
+    def assertASTsEquals(self, s, expectedlist, **parserargs):
+        results = parse(s, **parserargs)
         self.assertEquals(len(results), len(expectedlist),
                           'mismatch on ASTs length')
 
@@ -736,14 +736,14 @@ class test_parser(unittest.TestCase):
                                    redirect_kind, wordnode('EOF'))),
                     pipenode('|', '|'),
                     commandnode('b', wordnode('b', 'b'))),
-                  False)
+                  strictmode=False)
 
         s = 'a <<-b'
         self.assertASTEquals(s,
                 commandnode(s,
                   wordnode('a', 'a'),
                   redirectnode('<<-b', None, '<<-', wordnode('b'))),
-                False)
+                strictmode=False)
 
         s = 'a <<<<b'
         self.assertRaisesRegexp(errors.ParsingError, "unexpected token '<'.*5", parse, s)
@@ -886,3 +886,86 @@ class test_parser(unittest.TestCase):
                               reservedwordnode('done', 'done'),
                             ))
                           )
+
+    def test_expansion_limit(self):
+        '''make sure the expansion limit is working by tracking recursive
+        parsing count, and also checking that the word isn't expanded'''
+        counter = [0]
+        class countingparser(parser._parser):
+            def __init__(self, *args, **kwargs):
+                super(countingparser, self).__init__(*args, **kwargs)
+                counter[0] += 1
+
+        old = parser._parser
+        parser._parser = countingparser
+
+        try:
+            s = 'a $(b $(c $(d $(e))))'
+            self.assertASTEquals(s,
+                commandnode(s,
+                  wordnode('a'),
+                  wordnode('$(b $(c $(d $(e))))', '$(b $(c $(d $(e))))', [
+                    comsubnode('$(b $(c $(d $(e))))',
+                      commandnode('b $(c $(d $(e)))',
+                        wordnode('b'),
+                        wordnode('$(c $(d $(e)))')
+                      )
+                    )
+                  ])
+                ),
+                expansionlimit=1
+            )
+
+            self.assertEquals(counter[0], 3)
+        finally:
+            parser._parser = old
+
+        s = 'a $(b $(c))'
+        for i in [None] + range(2, 5):
+            self.assertASTEquals(s,
+                commandnode(s,
+                  wordnode('a'),
+                  wordnode('$(b $(c))', '$(b $(c))', [
+                    comsubnode('$(b $(c))',
+                      commandnode('b $(c)',
+                        wordnode('b'),
+                        wordnode('$(c)', '$(c)', [
+                          comsubnode('$(c)',
+                            commandnode('c',
+                              wordnode('c')
+                            )
+                          )
+                        ])
+                      )
+                    )
+                  ])
+                ),
+                expansionlimit=i
+            )
+
+    def test_expansion_limit_word(self):
+        s = 'a "$(b)"c" $1"'
+
+        self.assertASTEquals(s,
+            commandnode(s,
+              wordnode('a'),
+              wordnode('$(b)c $1', '"$(b)"c" $1"', [
+                comsubnode('$(b)',
+                  commandnode('b',
+                    wordnode('b'),
+                  )
+                ),
+                parameternode('$1', '$1'),
+              ])
+            ),
+        )
+
+        self.assertASTEquals(s,
+            commandnode(s,
+              wordnode('a'),
+              wordnode('$(b)c $1', '"$(b)"c" $1"', [
+                parameternode('$1', '$1'),
+              ])
+            ),
+            expansionlimit=0
+        )

commit 20519847fc7cc178aac7c85411bda1b85e10ec21

parser: move split() from tokenizer, pass parser to subst._expandwordinternal (fixes #12)
I broke this in 4e091dd and probably hadn't noticed because I didn't run the
doctests. Add a Makefile with a test target so I never forget.

Makefile
@@ -1,0 +1,5 @@
+tests:
+	@which nosetests 2>&1 >/dev/null || echo "error: nosetests missing, run 'pip install nose'\n"
+	nosetests --with-doctest tests/ bashlex/
+
+.PHONY: tests

bashlex/__init__.py
@@ -2,4 +2,4 @@ from . import parser, tokenizer
 
 parse = parser.parse
 parsesingle = parser.parsesingle
-split = tokenizer.split
+split = parser.split

bashlex/parser.py
@@ -610,6 +610,28 @@ def parse(s, strictmode=True, expansionlimit=None, convertpos=False):
 
     return parts
 
+def split(s):
+    '''a utility function that mimics shlex.split but handles more
+    complex shell constructs such as command substitutions inside words
+
+    >>> list(split('a b"c"\\'d\\''))
+    ['a', 'bcd']
+    >>> list(split('a "b $(c)" $(d) \\'$(e)\\''))
+    ['a', 'b $(c)', '$(d)', '$(e)']
+    >>> list(split('a b\\n'))
+    ['a', 'b', '\\n']
+    '''
+    p = _parser(s)
+    for t in p.tok:
+        if t.ttype == tokenizer.tokentype.WORD:
+            quoted = bool(t.flags & flags.word.QUOTED)
+            doublequoted = quoted and t.value[0] == '"'
+            parts, expandedword = subst._expandwordinternal(p, t, 0,
+                                                            doublequoted, 0, 0)
+            yield expandedword
+        else:
+            yield s[t.lexpos:t.endlexpos]
+
 class _parser(object):
     '''
     this class is mainly used to provide context to the productions

bashlex/tokenizer.py
@@ -1153,27 +1153,3 @@ class tokenizer(object):
 
         if self._parserstate & parserflags.CONDEXPR and tokstr == ']]':
             return tokentype.COND_END
-
-def split(s):
-    '''a utility function that mimics shlex.split but handles more
-    complex shell constructs such as command substitutions inside words
-
-    >>> list(split('a b"c"\\'d\\''))
-    ['a', 'bcd']
-    >>> list(split('a "b $(c)" $(d) \\'$(e)\\''))
-    ['a', 'b $(c)', '$(d)', '$(e)']
-    >>> list(split('a b\\n'))
-    ['a', 'b', '\\n']
-    '''
-    from bashlex import subst
-
-    tok = tokenizer(s, state.parserstate())
-    for t in tok:
-        if t.ttype == tokentype.WORD:
-            quoted = bool(t.flags & flags.word.QUOTED)
-            doublequoted = quoted and t.value[0] == '"'
-            parts, expandedword = subst._expandwordinternal(tok, t, 0,
-                                                            doublequoted, 0, 0)
-            yield expandedword
-        else:
-            yield s[t.lexpos:t.endlexpos]

setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bashlex',
-    version='0.9',
+    version='0.10',
     url='https://github.com/idank/bashlex.git',
     license='GPLv3+',
     author='Idan Kamara',